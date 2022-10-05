c15b7b2f173c67b5b27398b9666f641a276b9012
Weliton.lima <welitonlima159@gmail.com>
2022-10-05 18:44:00 -0300

    Requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      description: '',
+      image: '',
+      loadingUser: true,
+    };
+  }
+
+  componentDidMount() {
+    this.getProfileDate();
+  }
+
+  getProfileDate = async () => {
+    this.setState(
+      { loadingUser: true },
+      async () => {
+        const resposta = await getUser();
+        const { name, email, description, image } = resposta;
+        this.setState({
+          name,
+          email,
+          description,
+          image,
+          loadingUser: false,
+        });
+      },
+    );
+  };
+
   render() {
     const { compHeader } = this.props;
+    const { name, email, description, image, loadingUser } = this.state;
+    if (loadingUser === true) {
+      return <Carregando />;
+    }
     return (
       <div data-testid="page-profile">
         { compHeader }
+        <div>
+          <Link to="/profile/edit">Editar perfil</Link>
+          <img src={ image } alt="perfil" data-testid="profile-image" />
+          <h3>{name}</h3>
+          <p>{email}</p>
+          <p>{description}</p>
+        </div>
       </div>
     );
   }
